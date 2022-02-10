fa3480bd7f564c6f61c3bb1e76f9ccf8a1fbe704
monitoro <monitoro@gmail.com>
2022-02-10 16:32:04 +0900

    cube에서 잘못 fetch 된 파일 복구_CDC 중반부

--- src/ap/ap.c
+++ src/ap/ap.c
@@ -6,10 +6,15 @@
  */
 
 #include "ap.h"
+#include "led.h"
+#include "uart.h"
+//extern uint8_t CDC_Transmit_FS(uint8_t* Buf, uint16_t Len);
+
 
 
 void apInit(void)
 {
+	uartOpen(_DEF_UART1, 57600);
 
 
 }
@@ -17,11 +22,25 @@ void apInit(void)
 
 void apMain(void)
 {
+	uint32_t pre_time;
+	pre_time = millis();
+
 	while(1)
 	{
+		if (millis()-pre_time >= 500)
+		{
+			pre_time = millis();
+			ledToggle(_DEF_LED1);
+		}
 
-		ledToggle(_DEF_LED1);
-		delay(500);
+		if (uartAvailable(_DEF_UART1) > 0)
+		{
+			uint8_t rx_data;
+			rx_data = uartRead(_DEF_UART1);
+
+			uartPrintf(_DEF_UART1, "RxData : %c 0x%X\n", rx_data, rx_data);
+
+		}
 
 	}
 
